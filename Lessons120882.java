fedfe292ed7653c4396ab026869273def97561e0
JeonHS <tmd1007@naver.com>
2023-02-23 22:07:03 +0900

    Initial commit

--- Lessons120882.java
+++ Lessons120882.java
@@ -1,14 +1,10 @@
 import java.util.Arrays;
 
-public class Test {
-    public static void main(String[] args) {
-
-        Solution s = new Solution();
-
-    }
+public class Lessons120882 {
 }
 
-class Solution {
+
+class Solution120882 {
     public int[] solution(int[][] score) {
         int[] answer = new int[score.length];
         Arrays.fill(answer, 1);
